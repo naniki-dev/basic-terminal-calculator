8718d1330c4ab76374989640b6d45f9c7d63cfdd
LethaboKgasi <lethabomonyuku@gmail.com>
2026-02-21 12:50:29 +0200

    Added code to calculator.py

--- calculator.py
+++ calculator.py
@@ -1,18 +1,37 @@
 def main():
     print("This is a basic terminal calculator. You can only + - x / two sets of digits.")
-    sign = operator()
-    num_1 = first_num()
-    num_2 = sec_num()
-    return num_1, sign, num_2
+    sign = operator_check()
+    # num_1 = first_num()
+    # num_2 = sec_num()
+    if sign == "+":
+        add()
+    elif sign == "-":
+        minus()
+    elif sign == "/":
+        divide()
+    elif sign == "*":
+        times()
+    # else:
+    #     print("Enter a valid operator.")
 
 
 
-def first_num(n):
-    first = input("Enter the first number: ")
+def first_num():
+    try:
+        num = int(input("Enter the first number: "))
+        if not isinstance(num, int):
+            raise ValueError
+    except ValueError:
+        print("Only enter numbers.")
 
 
-def operator(n):
-    operator = input("Wyd? + - x /")
+def operator_check():
+    while True:
+        n = input("Wyd? (+ - x /) \n")
+        if n in ["+", "-", "/","*"]:
+            return n
+        else:
+            return "Enter a valid operator."
 
 
 def sec_num(n):
